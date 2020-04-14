1da14c949c37c29398984824c30d184cb3c59628
Ashish Kumar <rudrashishkumar@gmail.com>
2020-04-14 23:30:58 +0530

    Build folder

---
+++ build/precache-manifest.dd94d2281e5d7273a8b9a156c0743381.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbbc0aaeb11cfc8601497f94e94d9c77",
+    "url": "/react-library/index.html"
+  },
+  {
+    "revision": "b73dc2d47bf8f23c0767",
+    "url": "/react-library/static/css/main.98c47f12.chunk.css"
+  },
+  {
+    "revision": "98eb81ee8857272c44ed",
+    "url": "/react-library/static/js/2.35ac338d.chunk.js"
+  },
+  {
+    "revision": "78aa0676509153305bde072ed1e9cb1c",
+    "url": "/react-library/static/js/2.35ac338d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b73dc2d47bf8f23c0767",
+    "url": "/react-library/static/js/main.1ac45e46.chunk.js"
+  },
+  {
+    "revision": "f18af1a65f19533d130a",
+    "url": "/react-library/static/js/runtime-main.7b0a3d8e.js"
+  }
+]);
